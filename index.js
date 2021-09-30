abc4c5217d9eeb7ee44643e01c04a94110079bdc
ArtyomZakharenko <artiomzaharenko@gmail.com>
2021-09-30 20:16:19 +0300

    added Game class

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-'use strict'
+class Game {
+    constructor() {
+        this.startButton = document.querySelector('#start');
+        this.gameField = document.querySelector('#game');
+
+        this.startButton.addEventListener('click', () => {
+            this.startButton.classList.add('hide');
+            this.gameField.style.background = '#fff';
+        });
+
+    }
+
+
+}
+
+const game = new Game();
+
