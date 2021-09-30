1e94232187d858ca159bf0aa8c75028d1cc0c2c8
ArtyomZakharenko <artiomzaharenko@gmail.com>
2021-09-30 21:27:05 +0300

    added box rendering logic

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 class Game {
     constructor() {
+
         this.startButton = document.querySelector('#start');
         this.gameField = document.querySelector('#game');
 
-        this.startButton.addEventListener('click', () => {
-            this.startButton.classList.add('hide');
-            this.gameField.style.background = '#fff';
+        this.startButton.addEventListener('click', () => this.runGame());
+        this.gameField.addEventListener('click', (event) =>{
+            if (event.target.className === 'box') {
+                this.renderBox();
+                console.log('click on box');
+            }
         });
-
     }
 
+    runGame(){
+        this.startButton.classList.add('hide');
+        this.gameField.style.background = '#fff';
+        this.renderBox();
+        return this;
+    }
 
+    renderBox(){
+        this.gameField.innerHTML = '';
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.style.height = box.style.width = '50px';
+        box.style.background = '#000';
+        box.style.top = '50px';
+        box.style.left = '70px';
+
+        this.gameField.appendChild(box);
+    }
 }
 
 const game = new Game();
