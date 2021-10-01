a4ba375e08433315458aa7d9328b4dbd15e6428f
ArtyomZakharenko <artiomzaharenko@gmail.com>
2021-10-01 17:20:20 +0300

    added timer and result output

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 class Game {
     constructor() {
-
+        this.counter = 0;
+        this.isGameStarted = false;
         this.startButton = document.querySelector('#start');
         this.gameField = document.querySelector('#game');
+        this.time = document.querySelector('#time');
+        this.timerHeader = document.querySelector('#time-header');
+        this.resultHeader = document.querySelector('#result-header');
+        this.result = document.querySelector('#result');
 
         this.startButton.addEventListener('click', () => this.runGame());
         this.gameField.addEventListener('click', (event) =>{
-            if (event.target.className === 'box') this.renderBox();
+            if (!this.isGameStarted){
+                return;
+            }
+            if (event.target.className === 'box'){
+                this.renderBox();
+                this.counter++;
+            } 
         });
     }
 
+    setGameDuration (){
+        this.time.textContent = 5;
+    }
+
     runGame(){
+        this.counter = 0;
+        this.setGameDuration();
+        this.timerHeader.classList.remove('hide');
+        this.resultHeader.classList.add('hide');
+        this.isGameStarted = true;
         this.startButton.classList.add('hide');
         this.gameField.style.background = '#fff';
+        const interval = setInterval(() =>{
+            let time = parseFloat(this.time.textContent);
+            console.log();
+            
+        if (time > 0) {
+            this.time.textContent = (time - 0.1).toFixed(1);
+        } else {
+            clearInterval(interval);
+            this.endGame();
+        }
+
+        }, 100);
+
         this.renderBox();
     }
 
@@ -35,6 +68,16 @@ class Game {
 
     getRandom(min, max){
         return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    endGame(){
+        this.isGameStarted = false;
+        this.startButton.classList.remove('hide');
+        this.gameField.style.background = '#ccc';
+        this.gameField.innerHTML = '';
+        this.timerHeader.classList.add('hide');
+        this.resultHeader.classList.remove('hide');
+        this.result.textContent = this.counter + ' clicks';
     }
 }
 
